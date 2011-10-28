0894437410f19dbecc16ddf32d572a065ba1539b
Al Newkirk <we@ana.im>
2011-10-28 17:27:13 -0400

    Adjusted plugins attribute and the BUILD mechanism.

--- lib/Validation/Class/Validator.pm
+++ lib/Validation/Class/Validator.pm
@@ -98,9 +98,13 @@ has 'params' => (
 
 # class plugins store
 has plugins => (
-    is => 'rw',
-    isa => 'ArrayRef',
-    default => sub { [] }
+    is      => 'rw',
+    isa     => 'HashRef',
+    lazy    => 1,
+    default => sub {
+        return $_[0]->can('config') ?
+               $_[0]->config->{PLUGINS} : {};
+        }
 );
 
 # class relatives store
@@ -178,15 +182,15 @@ around BUILDARGS => sub {
 sub BUILD {
     my $self = shift;
     
-    # attach plugins
-    foreach my $plugin (@{$self->plugins}) {
-        $plugin->meta->apply($self);
-    }
-    
     # copy master (modified?) profile to config
     unless (keys %{$self->config}) {
         my $meta = $self->meta->find_attribute_by_name('config');
         $self->config($meta->profile);
+    }
+    
+    # attach plugins
+    foreach my $plugin (keys %{$self->plugins}) {
+        $plugin->meta->apply($self);
     }
     
     # automatically serialize params if nested hash detected
